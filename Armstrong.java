7a981f0678ce46a243d63b6f22c2f712fdacb51b
Prashant Kokate <115544507+mr-prashant-0094@users.noreply.github.com>
2024-03-26 12:22:51 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -1,1 +1,25 @@
+class Armstrong
+  {
+    public static void main(Sring[] args)
+    {
+      System.out.println("Enter a number ");
+      Scanner scan=new Scanner(System.in)
+       int num=scan.nextInt();
+      int rem, arm=0,number;
+      number=num;
+      while(num !=0)
+        {
+          rem=num%10;
+          arm=(rem*rem*rem)+arm;
+          num=num/10;
+        }
+      if(c==arm)
+      {
+        System.out.println("Armstrong number ");   
+      }
+      else
+      {
+        System.out.println("not Armstrong number");
+    }
+  }
 
