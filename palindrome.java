83472a1d1274be3687b0519ca5b463c30e3b4433
Prashant Kokate <115544507+mr-prashant-0094@users.noreply.github.com>
2024-03-26 11:41:53 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,1 +1,27 @@
-
+import java.util.*;
+class palindrome
+  {
+    public static void main(String[] args)
+    {
+      System.out.println("Enter a number ");
+      Scanner scan=new Scanner(System.in);
+      int num=scan.nextInt();
+      int i,rev,sum=0,number;
+      number=num;
+      while(num !=0)
+        {
+          rev=num%10;
+          sum=(sum*10)+rev;
+          num=num/10;
+        }
+           if(number==sum)
+           {
+             System.out.println(" Palindrome number ");
+           }
+         else
+           {
+             System.out.println("not palindrome number ");
+           }
+      
+    }
+  }
