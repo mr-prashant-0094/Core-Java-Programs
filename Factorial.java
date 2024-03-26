79bf6ae4af84daabe48912a4db2b156b6edf2163
Prashant Kokate <115544507+mr-prashant-0094@users.noreply.github.com>
2024-03-26 10:42:25 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,3 +1,14 @@
 class Factorial {
+  public static void main(String[] args)
+  {
+    int i,fact=1;
+    int number=5;
+    for( i=1;i<=number;i++)
+      {
+        fact=fact*i;
+      }
+    System.out.println("factorial of "+ number +" is "+fact);
+        
+  }
 
 }
