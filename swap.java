a1a9133c6b0d0fb01fb5af6adde4fedd32d13ef7
Prashant Kokate <115544507+mr-prashant-0094@users.noreply.github.com>
2024-03-28 18:10:57 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -1,1 +1,20 @@
+import java.util.*;
+class Snap
+  {
+      public static void main(String[] args)
+    {
+      Scanner scan=new Scanner(System.in);
+      int temp;
+      System.out.println("Enter a first number ");
+       int a = scan.nextInt();
+      System.out.println("Enter a second number ");
+      int b=scan.nextInt();
 
+      temp=a;
+      a=b;
+      b=temp;
+      System.out.println("first number "+ a );
+      System.out.println("Second number "+ b );   
+    }
+    
+  }
