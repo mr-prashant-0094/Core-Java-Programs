b8b36f6a9a4de27c3f6747d0870a3c267d8063f3
Prashant Kokate <115544507+mr-prashant-0094@users.noreply.github.com>
2024-03-28 18:00:56 +0530

    Update leap.java

--- leap.java
+++ leap.java
@@ -1,1 +1,22 @@
+  import java.util.*;
+ class Leap
+  {
+    public static void main(String[] args)
+    {
+          int y;
+          System.out.println("Enter a Year ");
+          Scanner scan=new Scanner(System.in);
+          y=scan.nextInt();
+    
+            if(y%100==0 && y%400==0 || y%100!=0 && y%4==0)
+              {
+                System.out.println(y+"this is leap year " );
+              }
+            else
+              {
+                System.out.println(y + " this is not leap year ");
+              }
+                
+    }
+  }
 
