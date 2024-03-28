7ecedbcc283c43ad718a5171472d026d564437a9
Prashant Kokate <115544507+mr-prashant-0094@users.noreply.github.com>
2024-03-28 18:17:11 +0530

    Update withoutThirdvariSwap.java

--- withoutThirdvariSwap.java
+++ withoutThirdvariSwap.java
@@ -1,1 +1,22 @@
+ import java.util.*;
+ class withoutThirdvariSwap {
 
+   public static void main (String[] args) {
+     Scanner scan=new Scanner(System.in);
+     System.out.println(" Enter first swap number ");
+     int x=scan.nextInt();
+     System.out.println("Enter second swap number ");
+     int y=scan.nextInt();
+
+      x=x+y;
+     y=x-y;
+     x=x-y;
+     System.out.println(" first number " + x);
+     System.out.println(" second number " + y);
+      
+     
+   }
+
+   
+   
+ }
